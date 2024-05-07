8cd631542ee71ce7724cc0916e4b414983666abd
Shoaib Ameeri <shoaibameeri56.com>
2024-05-08 00:38:44 +0500

    routes

---
+++ Routes/src/Components/mainx.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+
+Link
+
+function Main() {
+  return (
+  <BrowserRouter>
+    
+    <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/contact' element={<Contact/>} />
+      </Routes>
+
+   
+  </BrowserRouter>
+  )
+}
+
+export default Main
